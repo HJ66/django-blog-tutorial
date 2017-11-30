fe3473338a182e753a9822a947b3d35f4f6ce62a
HJ66 <2954962956@qq.com>
2017-11-30 21:25:09 +0800

    再次修改blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-class Category(models.Model):
-	'''
-    Django 要求模型必须继承 models.Model 类
-    Category 只需要一个简单的分类名 name 就可以了
-    CharField 指定了分类名 name 的数据类型，CharField是字符串类型
-    CharField 的 max_length 参数指定其最大长度，超过这个长度的name就不能写入数据库
-    当然 Django 还为我们提供了多种其它的数据类型，如日期时间类型 DateTimeField、整数类型 IntegerField 等等。
-    Django 内置的全部类型可查看文档：
-    https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
-	'''
-    
+class Category(models.Model):    
     name = models.CharField(max_length=100)
 
     def __str__(self):
